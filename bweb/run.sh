09b0246b5349ecb875aed02329182840cf420bb9
StanimirVelikov <s.velikof@gmail.com>
2016-07-27 16:52:14 +0300

    now docker-compose run normally
    BWEB
    add bacula libs to run baculus in docker file
    fix permissions
    bweb need to reconfigure when run docker localhost:9095

--- bweb/run.sh
+++ bweb/run.sh
@@ -1,3 +1,6 @@
-source /etc/apache2/envvars 
-service apache2 start
-/bin/bash
+cp -R /tmp/lib/ /usr/
+chown www-data:www-data /usr/share/baculum/htdocs/protected/Data/settings.conf
+chmod 700 /usr/share/baculum/htdocs/protected/Data/settings.conf
+service apache2 start 
+bash
+
